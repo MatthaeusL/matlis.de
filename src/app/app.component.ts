329d278ce3fe0636e0432a7449b01621aa6c0210
MatthaeusL <mat@lispage.com>
2022-04-06 11:08:53 +0200

    Added Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
- import { Component } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'matlis.de';
+export class AppComponent implements OnInit{
+  images = ['pexels1.jpg','pexels2.jpg','pexels3.jpg'];
+  currentImage=0;
+  showImage = true; 
+
+  ngOnInit(){
+    this.updateImage();
+
+  }
+
+  updateImage() {
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+        
+      }, 10);
+
+    },7000)
+
+    
+  }
+
 }
